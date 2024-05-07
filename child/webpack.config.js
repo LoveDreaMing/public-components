dd045299f78e2725c1a7b046d045eb35b8d75514
huangjiaming04 <huangjiaming04@58.com>
2024-05-07 19:40:27 +0800

    feat: 配置polyfill

--- child/webpack.config.js
+++ child/webpack.config.js
@@ -73,7 +73,15 @@ const config = {
                     {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['@babel/preset-env']
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        corejs: '3',
+                                        useBuiltIns: 'usage'
+                                    }
+                                ]
+                            ]
                         }
                     }
                 ]
